961402ddf6cd43276830a0aadcfc0224f91f0847
vaibhavi-coditas <vaibhavi.varma@coditas.com>
2021-06-02 18:45:52 +0530

    commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  @Input()  List : any = [];
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.List);
   }
-
+  onDelete(user:any){
+    const index : number= this.List.indexOf(user);
+    if(index >- 1){
+      this.List.splice(index, 1)
+    }
+  }
+  onEdit(user:any ){
+    var index = this.List.indexOf(3452);
+    if(index !== -1){
+      this.List[index]=1010;
+    }
+  }
 }
